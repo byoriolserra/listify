49ac90eee412acdcdfb878b73d3154e3235ff6ab
Oriol Serra <87442357+byoriolserra@users.noreply.github.com>
2021-12-31 18:44:33 +0100

    Create Navbar Component

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Navbar from './components/Navbar';
 
 describe('App', () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  })
+
   it('renders without crashing', () => {
-    const wrapper = shallow(<App />);
 
     expect(wrapper).toHaveLength(1);
+  })
+
+  it('renders a navbar', () => {
+    const navbar = wrapper.find(Navbar);
+
+    expect(navbar).toHaveLength(1);
   })
 })
